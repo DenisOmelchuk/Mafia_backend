467b1d9c2610a32437eaf23e2dd98a620c1fbd92
Denys-Omelchuk <softwared757@gmail.com>
2024-05-08 04:03:14 +0300

    Add models and mofidy some files:     - Create CustomUser model     - Create RoomMember model     - Register CustomUser and RoomMember models in admin.py     - Create API/urls.py file     - Register API/urls.py in Mafia_backend/ulrs.py file

--- API/admin.py
+++ API/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import RoomMember, CustomUser
 
-# Register your models here.
+admin.site.register(RoomMember)
+admin.site.register(CustomUser)
--- API/models.py
+++ API/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=20, null=True, unique=True)
+    avatar = models.ImageField(null=True, blank=False)
+
+    REQUIRED_FIELDS = ['avatar']
+
+
+class RoomMember(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models)
+    token = models.CharField(max_length=1000)
+    chanel = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.chanel
