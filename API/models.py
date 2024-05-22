b9736310b099c510e59400b07fdcb557c9a23bd4
Denys-Omelchuk <softwared757@gmail.com>
2024-05-23 00:55:06 +0300

    Add API endpoint for user-registration and handle saving user instance to db

--- API/models.py
+++ API/models.py
@@ -8,11 +8,15 @@ class CustomUser(AbstractUser):
 
     REQUIRED_FIELDS = ['avatar']
 
+    def __str__(self):
+        return self.username
+
 
 class RoomMember(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     token = models.CharField(max_length=1000)
-    chanel = models.CharField(max_length=200)
+    channel = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.chanel
+        return f"{self.user.username} in {self.channel}"
+
--- API/views.py
+++ API/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from rest_framework_simplejwt.tokens import RefreshToken
+from API.serializers import RegisterUserSerializer
+from .models import CustomUser
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+@api_view(['POST'])
+def registration(request):
+    serializer = RegisterUserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        username = serializer.validated_data['username']
+        user = CustomUser.objects.get(username=username)
+        login(request, user)
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
