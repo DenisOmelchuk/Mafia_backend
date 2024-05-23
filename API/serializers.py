88d850fba62b31d44db84be2b035e2cc411d5105
Denys-Omelchuk <softwared757@gmail.com>
2024-05-23 16:53:00 +0300

    Modify user-registration endpoint     - Add password setting

--- API/serializers.py
+++ API/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
 from API.models import RoomMember, CustomUser
+from django.contrib.auth import authenticate
 
 
 class RegisterUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = '__all__'
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create(
+            username=validated_data['username'],
+            avatar=validated_data['avatar']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+
--- API/views.py
+++ API/views.py
@@ -22,3 +22,6 @@ def registration(request):
         }, status=status.HTTP_200_OK)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+
+
