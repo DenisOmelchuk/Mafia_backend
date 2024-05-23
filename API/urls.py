dd8251b66c3749569f0631351c395a6e4ca53fab
Denys-Omelchuk <softwared757@gmail.com>
2024-05-23 16:57:13 +0300

    Add user login endpoint

--- API/urls.py
+++ API/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import registration
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('registration/', registration, name='register'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
