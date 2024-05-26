c1fe1b0cc3132603dcbd1c3eb690f6e8078d580b
Denys-Omelchuk <softwared757@gmail.com>
2024-05-26 19:03:48 +0300

    Add user avatars temprorary image storage     - set media folder for image storage     - add media folder to project

--- API/urls.py
+++ API/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from .views import registration
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('registration/', registration, name='register'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
